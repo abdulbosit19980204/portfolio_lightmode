0d0dca734feda1a65a0807dfe07e48946c49d0f2
Abdulbosit Tuychiev <71276545+abdulbosit19980204@users.noreply.github.com>
2023-04-03 22:30:41 +0000

    bug is changed

--- assets/js/printShape.js
+++ assets/js/printShape.js
@@ -12,19 +12,20 @@ function skillval() {
     for(let i = 0; i<printShape.length; i++){
         for(let j=0; j<progressval.length;i++){
             let pariavalNow = +progressval[i].ariaValueNow
-            let rightPosition = (skillProgressWrap- ((skillProgressWrap*pariavalNow)/100))+"px"
+            let rightPosition = ((skillProgressWrap- ((skillProgressWrap*pariavalNow)/100))-5)
+            console.log(rightPosition)
             if (pariavalNow > 70) {
                 // printShape[i].style.backgroundColor = '#0000FF'
                 printShape[i].style.backgroundColor = '#1100FF'
-                printShape[i].style.right =rightPosition
+                printShape[i].style.right =rightPosition+"px"
 
-            } else if ((pariavalNow > 50) && (pariavalNow <= 70)) {
+            } else if ((pariavalNow > 30) && (pariavalNow <= 70)) {
                 printShape[i].style.backgroundColor = '#0044FF'
-                printShape[i].style.right =rightPosition
+                printShape[i].style.right =rightPosition+"px"
             } else {
                 printShape[i].style.backgroundColor = '#0066FF'
-                printShape[i].style.right =rightPosition
-    
+                printShape[i].style.right =rightPosition-100+"px"
+                console.log("20% kamlardiki => ",rightPosition);
             }
         }
            }
